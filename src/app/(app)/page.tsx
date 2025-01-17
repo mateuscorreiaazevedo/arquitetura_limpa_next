92c5e325b1ddf842ff085eb1d98ac26eba46b30e
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2025-01-17 19:13:26 -0300

    chore(main): rename folders

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,14 +1,14 @@
-import { postFactory } from "@/main/factories/post";
-import { PostItem } from "./_components/post";
+import { postFactory } from '@/main/factories/post'
+import { PostItem } from './_components/post'
 
 export default async function App() {
-  const { data } = await postFactory.getAllPosts.execute();
+  const { data } = await postFactory.getAllPosts.execute()
 
   return (
     <div className="flex flex-col gap-2 p-4">
-      {data.map((item) => (
+      {data.map(item => (
         <PostItem key={item.postId} {...item} />
       ))}
     </div>
-  );
+  )
 }
