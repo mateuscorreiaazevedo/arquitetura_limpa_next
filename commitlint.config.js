efd64651040f1990f92231e306755f9c1348c6b5
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2025-01-18 01:52:51 -0300

    wip(developer): access level

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2, // nível de severidade (2 é erro)
+      'always', // aplicar a regra sempre
+      [
+        'feat', // Novas funcionalidades
+        'fix', // Correções de bugs
+        'wip', // Trabalho em progresso
+        'chore', // Atualizações menores e manutenção
+        'refactor', // Refatoração de código
+        'style', // Alterações de estilo (semântico ou visual)
+        'docs', // Atualizações de documentação
+      ],
+    ],
+  },
 }
